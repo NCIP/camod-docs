39f24868d2f0ee3d56a4a8755ef5109ef70bc2c0
Sima Pandya <pandyas@mail.nih.gov>
2006-10-23 14:29:47 +0000

    added help files to work on laptop
    SVN-Revision: 2422

---
+++ artifacts/TODO/Output/WebWorks Help 5.0/caMOD 2.2 OH/wwhdata/common/context.js
@@ -0,0 +1,4 @@
+function  WWHBookData_Context()
+{
+  return "caMOD_2_2_OH";
+}
---
+++ artifacts/TODO/Output/WebWorks Help 5.0/caMOD 2.2 OH/wwhdata/js/search/pairs/pair70.js
@@ -0,0 +1,18 @@
+function FileData_Pairs(x)
+{
+x.t("searching","camod");
+x.t("trials","searching");
+x.t("trials","pre");
+x.t("trials","populated");
+x.t("camod","database");
+x.t("drug","screening");
+x.t("pre","clinical");
+x.t("stored","camod");
+x.t("populated","data");
+x.t("search","pre");
+x.t("database","searching");
+x.t("database","drug");
+x.t("data","stored");
+x.t("screening","search");
+x.t("clinical","trials");
+}
