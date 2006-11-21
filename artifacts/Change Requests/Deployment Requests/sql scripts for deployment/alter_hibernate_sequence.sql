7cade012c582f34056201f256c22c221677256fc
Sima Pandya <pandyas@mail.nih.gov>
2006-11-21 17:40:38 +0000

    newest versions of DB scripts
    SVN-Revision: 2542

---
+++ artifacts/Change Requests/Deployment Requests/sql scripts for deployment/alter_hibernate_sequence.sql
@@ -0,0 +1,5 @@
+DROP SEQUENCE hibernate_sequence;
+
+CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 50000000;
+
+commit;
---
+++ artifacts/Change Requests/Deployment Requests/sql scripts for deployment/dropUniqueConstraint.sql
@@ -0,0 +1,16 @@
+-- drop unique constraint
+declare
+begin
+   for c1 in (
+     select uc.constraint_name theName, ucc.column_name
+       from user_constraints uc,
+            user_cons_columns ucc
+      where uc.constraint_type = 'U' 
+        and uc.table_name = 'ABS_CAN_MOD_PUBLICATION'
+        and uc.constraint_name = ucc.constraint_name
+        and ucc.column_name = 'PUBLICATION_ID'
+   ) loop
+       execute immediate 'alter table ABS_CAN_MOD_PUBLICATION drop constraint ' || c1.theName;
+   end loop;
+end;
+/
