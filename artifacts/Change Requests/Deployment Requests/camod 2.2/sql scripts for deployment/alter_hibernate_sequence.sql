50fc298427b3b330d8d06312259bb77c79c4139f
Sima Pandya <pandyas@mail.nih.gov>
2006-11-29 21:09:13 +0000

    increased sequence start after testing
    SVN-Revision: 2588

--- artifacts/Change Requests/Deployment Requests/camod 2.2/sql scripts for deployment/alter_hibernate_sequence.sql
+++ artifacts/Change Requests/Deployment Requests/camod 2.2/sql scripts for deployment/alter_hibernate_sequence.sql
@@ -1,5 +1,5 @@
 DROP SEQUENCE hibernate_sequence;
 
-CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 50000000;
+CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 50005000;
 
 commit;
