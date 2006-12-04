e5e41b98ab8b0ccf5d9300dc5060af05546bf09e
Sima Pandya <pandyas@mail.nih.gov>
2006-12-04 15:31:20 +0000

    Increased sequence to account for the high number of models submitted in the last month.   Histopathology went from using the max number under 50000000 to using 50025856 between the time we tested on dev to the time we built to production.
    SVN-Revision: 2603

--- artifacts/Change Requests/Deployment Requests/camod 2.2/sql scripts for deployment/alter_hibernate_sequence.sql
+++ artifacts/Change Requests/Deployment Requests/camod 2.2/sql scripts for deployment/alter_hibernate_sequence.sql
@@ -1,5 +1,5 @@
 DROP SEQUENCE hibernate_sequence;
 
-CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 50005000;
+CREATE SEQUENCE hibernate_sequence INCREMENT BY 1 START WITH 50050000;
 
 commit;
