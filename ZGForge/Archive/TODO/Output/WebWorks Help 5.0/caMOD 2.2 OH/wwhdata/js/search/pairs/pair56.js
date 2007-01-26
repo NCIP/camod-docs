86880e0c03d8e54c9ce6b70752eb9fe13bdcc754
Sima Pandya <pandyas@mail.nih.gov>
2007-01-26 21:01:36 +0000

    Re-org folders and files for GForge move
    SVN-Revision: 2635

---
+++ ZGForge/Archive/TODO/Output/WebWorks Help 5.0/caMOD 2.2 OH/wwhdata/js/search/pairs/pair56.js
@@ -0,0 +1,10 @@
+function FileData_Pairs(x)
+{
+x.t("searching","camod");
+x.t("camod","database");
+x.t("includes","following");
+x.t("section","includes");
+x.t("database","searching");
+x.t("database","section");
+x.t("following","topics:");
+}
