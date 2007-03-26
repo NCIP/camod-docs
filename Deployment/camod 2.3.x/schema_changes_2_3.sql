3539c7cf376a5ea26596572e10edeab7e10a37e6
Sima Pandya <pandyas@mail.nih.gov>
2007-03-26 12:04:50 +0000

    Deployment for bug fix and changes for caMOD 2.3
    SVN-Revision: 2716

--- Deployment/camod 2.3.x/schema_changes_2_3.sql
+++ Deployment/camod 2.3.x/schema_changes_2_3.sql
@@ -1,4 +1,5 @@
 -- remove and reload jax data with EF.type values for CI
+-- Protect disease, organ, microarray, ect.
 
 
 
@@ -12,10 +13,10 @@ ADD rgd_number VARCHAR2(255);
 
 -- Add MGI” as prefix to mgi_number value
 UPDATE MUTATION_IDENTIFIER MI
-SET MI.MGI_NUMBER = ‘mgi:’ || MI.MGI_NUMBER;
+SET MI.MGI_NUMBER = ‘MGI:’ || MI.MGI_NUMBER;
 
--- Add column named developmental_stage to Animal_Model
-alter table Animal_Model
+-- Add column named developmental_stage to abs_cancer_model (for Animal_Model)
+alter table abs_cancer_model
 ADD developmental_stage  VARCHAR2(255);
 
 -- Add column named site to Transient_Interference
@@ -31,15 +32,14 @@ alter table Therapy
 ADD developmental_stage VARCHAR2(255);
 
 -- Add column named conditioning_regime  to Abs_Cancer _Model
-alter table Abs_Cancer _Model
+alter table Abs_Cancer_Model
 ADD conditioning_regime  VARCHAR2(255);
 
 -- Add column named cond_regime_unctrl_vocab to Abs_Cancer _Model
-alter table Abs_Cancer _Model
+alter table Abs_Cancer_Model
 ADD cond_regime_unctrl_vocab  VARCHAR2(255);
 
 -- Update stock_number add pre-ceeding 0's
-
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002020' WHERE ABS_CANCER_MODEL_ID = '49';
 
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002373' WHERE ABS_CANCER_MODEL_ID = '86';
@@ -57,8 +57,9 @@ UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002376' WHERE ABS_CANCER_MODEL_ID
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002373' WHERE ABS_CANCER_MODEL_ID = '434';
 
 -- Fix rows with null PI records
-
-
-
+UPDATE ABS_CANCER_MODEL ac 
+SET ac.PRINCIPAL_INVESTIGATOR_ID = '56' 
+where ac.ABS_CANCER_MODEL_TYPE = 'AM'
+and ac.PRINCIPAL_INVESTIGATOR_ID IS NULL;
 
 
