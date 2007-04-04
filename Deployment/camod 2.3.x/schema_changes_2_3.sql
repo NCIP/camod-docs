1d1a47ad427eed9a66cb031f0eef5631b87dadd8
Sima Pandya <pandyas@mail.nih.gov>
2007-04-04 13:29:40 +0000

    Modified name for conditioning regimen,  target site, all mutation identifier names
    SVN-Revision: 2753

--- Deployment/camod 2.3.x/schema_changes_2_3.sql
+++ Deployment/camod 2.3.x/schema_changes_2_3.sql
@@ -1,20 +1,24 @@
 -- Modify EF.type values instead of reloading data
--- Protect disease, organ, microarray, ect.
+
+-- Synronize type = Chemical / Drug
 UPDATE environmental_factor ef 
 SET ef.TYPE_UNCTRL_VOCAB = 'Chemical / Drug' 
 where ef.TYPE_UNCTRL_VOCAB = 'Chemical/Drug' 
 and ef.TYPE_UNCTRL_VOCAB IS NOT NULL;
 
+-- Synronize type = Growth Factor
 UPDATE environmental_factor ef 
 SET ef.TYPE_UNCTRL_VOCAB = 'Growth Factor' 
 where ef.TYPE_UNCTRL_VOCAB = 'Growth Factor Type' 
 and ef.TYPE_UNCTRL_VOCAB IS NOT NULL;
 
+-- Synronize type = Hormone
 UPDATE environmental_factor ef 
 SET ef.TYPE_UNCTRL_VOCAB = 'Hormone' 
 where ef.TYPE_UNCTRL_VOCAB = 'Hormone Type' 
 and ef.TYPE_UNCTRL_VOCAB IS NOT NULL;
 
+-- Synronize type = Other
 UPDATE environmental_factor ef 
 SET ef.TYPE_UNCTRL_VOCAB = 'Other' 
 where ef.TYPE_UNCTRL_VOCAB = 'Other Type' 
@@ -22,31 +26,36 @@ and ef.TYPE_UNCTRL_VOCAB IS NOT NULL;
 
 -- Radiation Type in newest QA, Stage and Prod script - not in dev
 
-
+-- Synronize type = Viral
 UPDATE environmental_factor ef 
 SET ef.TYPE_UNCTRL_VOCAB = 'Viral' 
 where ef.TYPE_UNCTRL_VOCAB = 'Virus' 
 and ef.TYPE_UNCTRL_VOCAB IS NOT NULL;
 
--- Add column named zfin_Number to Mutation_Identifier
-alter table Mutation_Identifier
-ADD zfin_Number VARCHAR2(255);
-
--- Add column named rgd_number to Mutation_Identifier
-alter table Mutation_Identifier
-ADD rgd_number VARCHAR2(255);
-
--- Add MGI” as prefix to mgi_number value
+-- Add MGI as prefix to mgi_number value
 UPDATE MUTATION_IDENTIFIER MI
 SET MI.MGI_NUMBER = 'MGI:' || MI.MGI_NUMBER;
+
+-- Add column named zfin_id to Mutation_Identifier
+alter table Mutation_Identifier
+ADD zfin_Id VARCHAR2(255);
+
+-- Add column named rgd_id to Mutation_Identifier
+alter table Mutation_Identifier
+ADD rgd_Id VARCHAR2(255);
+
+-- Change column name from mgi_number to mgi_id
+ALTER TABLE Mutation_Identifier
+RENAME COLUMN mgi_number to mgi_id;
+
 
 -- Add column named developmental_stage to abs_cancer_model (for Animal_Model)
 alter table abs_cancer_model
 ADD developmental_stage  VARCHAR2(255);
 
--- Add column named site to Transient_Interference
+-- Add column named targetSite to Transient_Interference
 alter table Transient_Interference
-ADD site VARCHAR2(255);
+ADD target_site VARCHAR2(255);
 
 -- Insert animal distributor for models from zfin
 INSERT INTO ANIMAL_DISTRIBUTOR (ANIMAL_DISTRIBUTOR_ID, NAME )
@@ -60,13 +69,13 @@ VALUES(7, 'Small Molecule');
 alter table Therapy
 ADD developmental_stage VARCHAR2(255);
 
--- Add column named conditioning_regime  to Abs_Cancer _Model
+-- Add column named conditioning_regimen  to Abs_Cancer _Model
 alter table Abs_Cancer_Model
-ADD conditioning_regime  VARCHAR2(255);
+ADD conditioning_regimen  VARCHAR2(255);
 
--- Add column named cond_regime_unctrl_vocab to Abs_Cancer _Model
+-- Add column named cond_regimen_unctrl_vocab to Abs_Cancer _Model
 alter table Abs_Cancer_Model
-ADD cond_regime_unctrl_vocab  VARCHAR2(255);
+ADD cond_regimen_unctrl_vocab  VARCHAR2(255);
 
 -- Update stock_number add pre-ceeding 0's
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002020' WHERE ABS_CANCER_MODEL_ID = '49';
@@ -81,7 +90,7 @@ UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002020' WHERE ABS_CANCER_MODEL_ID
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002376' WHERE ABS_CANCER_MODEL_ID = '343';
 UPDATE ANIMAL_AVAILABILITY SET STOCK_NUMBER = '002373' WHERE ABS_CANCER_MODEL_ID = '434';
 
--- Fix rows with null PI records
+-- Fix rows with null PI records - set PI to Ulli Wagner to fix
 UPDATE ABS_CANCER_MODEL ac 
 SET ac.PRINCIPAL_INVESTIGATOR_ID = '56' 
 where ac.ABS_CANCER_MODEL_TYPE = 'AM'
