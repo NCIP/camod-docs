52130c9bff84b55a0989a5ab86bb3744d00bb889
Sima Pandya <pandyas@mail.nih.gov>
2007-04-04 13:30:30 +0000

    Changed experiemnt id from excel scientific number to string of nubmers for Oracle to convert
    SVN-Revision: 2754

--- Deployment/camod 2.2.x/camod 2.2.1/Bug_Fix_Build/AddMicroArray.sql
+++ Deployment/camod 2.2.x/camod 2.2.1/Bug_Fix_Build/AddMicroArray.sql
@@ -1,26 +1,26 @@
 INSERT INTO MICRO_ARRAY_DATA ( MICRO_ARRAY_DATA_ID, EXPERIMENT_NAME, EXPERIMENT_ID,
 OTHER_LOCATION_URL, ABS_CANCER_MODEL_ID ) VALUES ( 
 6, 'Identification of large-scale embryonic and oncogenic transcriptional modules in human colon cancer using mouse models comparisons'
-, 1.01589759091347E15, NULL, 50050295);
+, 1015897590913471, NULL, 50050295);
  
 INSERT INTO MICRO_ARRAY_DATA ( MICRO_ARRAY_DATA_ID, EXPERIMENT_NAME, EXPERIMENT_ID,
 OTHER_LOCATION_URL, ABS_CANCER_MODEL_ID ) VALUES ( 
 7, 'Identification of large-scale embryonic and oncogenic transcriptional modules in human colon cancer using mouse models comparisons'
-, 1.01589759091347E15, NULL, 49); 
+, 1015897590913471, NULL, 49); 
 
 INSERT INTO MICRO_ARRAY_DATA ( MICRO_ARRAY_DATA_ID, EXPERIMENT_NAME, EXPERIMENT_ID,
 OTHER_LOCATION_URL, ABS_CANCER_MODEL_ID ) VALUES ( 
 8, 'Identification of large-scale embryonic and oncogenic transcriptional modules in human colon cancer using mouse models comparisons'
-, 1.01589759091347E15, NULL, 50000765); 
+, 1015897590913471, NULL, 50000765); 
 
 INSERT INTO MICRO_ARRAY_DATA ( MICRO_ARRAY_DATA_ID, EXPERIMENT_NAME, EXPERIMENT_ID,
 OTHER_LOCATION_URL, ABS_CANCER_MODEL_ID ) VALUES ( 
 9, 'Identification of large-scale embryonic and oncogenic transcriptional modules in human colon cancer using mouse models comparisons'
-, 1.01589759091347E15, NULL, 301); 
+, 1015897590913471, NULL, 301); 
 
 INSERT INTO MICRO_ARRAY_DATA ( MICRO_ARRAY_DATA_ID, EXPERIMENT_NAME, EXPERIMENT_ID,
 OTHER_LOCATION_URL, ABS_CANCER_MODEL_ID ) VALUES ( 
-10, 'Murine KRASLA lung cancer gene expression', 1.01589759023117E15, NULL, 147); 
+10, 'Murine KRASLA lung cancer gene expression', 1015897590231167, NULL, 147); 
 
 commit;
  
