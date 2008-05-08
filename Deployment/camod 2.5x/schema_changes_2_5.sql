47834f0a2392e3dcda03c232bdaafe668b7ceea9
Sima Pandya <pandyas@mail.nih.gov>
2008-05-08 18:41:32 +0000

    SQL for caMOD 2.5 build
    SVN-Revision: 3491

--- Deployment/camod 2.5x/schema_changes_2_5.sql
+++ Deployment/camod 2.5x/schema_changes_2_5.sql
@@ -3,3 +3,17 @@
 update species sp
 set sp.SCIENTIFIC_NAME = 'Equus caballus'
 where sp.SPECIES_ID = 11;
+
+-- #10654 Clean up contact_info set e-mail to null
+UPDATE CONTACT_INFO
+SET EMAIL = Null
+WHERE EMAIL IS NOT NULL;
+
+--  #13898 Renamed OtherLocationURL in micro array data object to url
+ALTER TABLE MICRO_ARRAY_DATA
+RENAME COLUMN OTHER_LOCATION_URL to URL;
+
+-- Add flag named is_Induced_Mutation_Trigger to ENVIRONMENTAL_FACTOR
+alter table ENVIRONMENTAL_FACTOR
+ADD is_Induced_Mutation_Trigger       NUMBER(1);
+
