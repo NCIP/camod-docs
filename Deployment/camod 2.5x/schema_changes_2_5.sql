b7eee4cc82932336fc9b94208192299460093071
Sima Pandya <pandyas@mail.nih.gov>
2008-05-22 18:25:21 +0000

    Added clean up for species.scientific_name so SQL injection validation can work correctly
    SVN-Revision: 3514

--- Deployment/camod 2.5x/schema_changes_2_5.sql
+++ Deployment/camod 2.5x/schema_changes_2_5.sql
@@ -17,3 +17,36 @@ RENAME COLUMN OTHER_LOCATION_URL to URL;
 alter table ENVIRONMENTAL_FACTOR
 ADD is_Induced_Mutation_Trigger       NUMBER(1);
 
+-- Remove the trailing space after the scientific name for species
+update species sp
+set sp.SCIENTIFIC_NAME = 'Meriones unguiculatus'
+where sp.SPECIES_ID = 5;
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Mesocricetus auratus'
+where sp.SPECIES_ID = 6;
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Cavia porcellus'
+where sp.SPECIES_ID = 7;
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Bos taurus'
+where sp.SPECIES_ID = 8;
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Canis familiaris'
+where sp.SPECIES_ID = 9;
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Capra hircus'
+where sp.SPECIES_ID = 10; 
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Equus Caballus'
+where sp.SPECIES_ID = 11; 
+
+update species sp
+set sp.SCIENTIFIC_NAME = 'Ovis aries'
+where sp.SPECIES_ID = 12; 
+
