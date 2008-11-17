fccc672cc73f97641afa3005e75252e9ba1ab76d
Sima Pandya <pandyas@mail.nih.gov>
2008-11-17 05:27:03 +0000

    Removed SQL to clear e-mail and delete e-mail column from database.  We have to store e-mails again since LDAP is deleting inactive accounts now.
    SVN-Revision: 3889

--- Deployment/camod 2.5x/schema_changes_2_5.sql
+++ Deployment/camod 2.5x/schema_changes_2_5.sql
@@ -4,14 +4,6 @@ update species sp
 set sp.SCIENTIFIC_NAME = 'Equus caballus'
 where sp.SPECIES_ID = 11;
 
--- #10654 Clean up contact_info set e-mail to null
-UPDATE CONTACT_INFO
-SET EMAIL = Null
-WHERE EMAIL IS NOT NULL;
-
--- Remove EMAIL from CONTACT_INFO in another release (caDSR registered must match grid schema)
---Alter table CONTACT_INFO drop column EMAIL;
-
 --  #13898 Renamed OtherLocationURL in micro array data object to url
 ALTER TABLE MICRO_ARRAY_DATA
 RENAME COLUMN OTHER_LOCATION_URL to URL;
