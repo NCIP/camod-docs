59b1f54e02af266913b671d759b703f3cc040f89
Sima Pandya <pandyas@mail.nih.gov>
2009-02-03 18:53:14 +0000

    fix #19107  	Clean up unused tables in database - backup and tmp tables
    SVN-Revision: 4053

--- Deployment/camod 2.5x/2.5.1/schema_changes_2_5.sql
+++ Deployment/camod 2.5x/2.5.1/schema_changes_2_5.sql
@@ -4,8 +4,27 @@ update BIOLOGICAL_PROCESS bp
 set bp.NAME = 'Adhesion, Motility, Invasion, Metastasis'
 where bp.BIOLOGICAL_PROCESS_ID = 1;
 
-
-
+-- Drop unused tables from database - clean up on all tiers
+drop table ABS_CANCER_MODEL_TMP;
+drop table DISEASE_MAPPING_STG;
+drop table DISEASE_STG;
+drop table ENV_CAS_NSC_STG;
+drop table GENOTYPE_BKP;
+drop table JACKSON_LAB_TMP;
+drop table MGITEMP;
+drop table MTB_ORGAN_MAP;
+drop table MTB_PUBLICATION;
+drop table ORGAN_MAPPING_STG;
+drop table PUBLICATION_STG;
+drop table REPOSITORY_INFO_BKP;
+drop table SPECIES_STG;
+drop table SPECIES_STRAIN_STG;
+drop table STAIN2IMAGE_STG;
+drop table STAINING_STG;
+drop table STRAIN_RATTUS_STG;
+drop table STRAIN_STG;
+drop table TAXON_MAPPING_STG;
+drop table XENOGRAFT_INVIVO_RESULT_BKP;
 
 commit;
 
