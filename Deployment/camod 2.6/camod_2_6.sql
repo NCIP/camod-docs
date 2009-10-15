f982e82025a2291695b850967a3b9e995b10b9e9
Sima Pandya <pandyas@mail.nih.gov>
2009-10-15 17:08:25 +0000

    added sql for name and nameAlternEntry ran successfully on dev server
    SVN-Revision: 4798

--- Deployment/camod 2.6/camod_2_6.sql
+++ Deployment/camod 2.6/camod_2_6.sql
@@ -2,33 +2,44 @@ update ENVIRONMENTAL_FACTOR ef
 set ef.IS_INDUCED_MUTATION_TRIGGER = '0'
 where ef.IS_INDUCED_MUTATION_TRIGGER is null;
 
---  does not work yet
+-- ran on dev manually - will automate in BDA (Fixed 1730 rows)
 update ENVIRONMENTAL_FACTOR ef
 set ef.TYPE = ef.TYPE_ALTERN_ENTRY
 where ef.TYPE is null
-and ef.ENVIRONMENTAL_FACTOR_ID is IN (
-
--- returns 4023 rows, I manually changed 7 rows on dev
-select ef.ENVIRONMENTAL_FACTOR_ID, ef.TYPE, ef.TYPE_ALTERN_ENTRY, ef.NAME, ef.NAME_ALTERN_ENTRY, ac.EXTERNAL_SOURCE
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+and ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY;
+and ac.EXTERNAL_SOURCE = 'Jax MTB');
 
-
--- then null out the typeAlternEntry field
+-- then null out the typeAlternEntry field (Fixed 1730 rows)
 update ENVIRONMENTAL_FACTOR ef
 set ef.TYPE_ALTERN_ENTRY = null
-where ef.ENVIRONMENTAL_FACTOR_ID is IN (
-
--- returns 4023 rows, I manually changed 7 rows on dev
-select ef.ENVIRONMENTAL_FACTOR_ID, ef.TYPE, ef.TYPE_ALTERN_ENTRY, ef.NAME, ef.NAME_ALTERN_ENTRY, ac.EXTERNAL_SOURCE
-from environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce
+where ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
 where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
 and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
-and ac.EXTERNAL_SOURCE = 'Jax MTB'
-order by ef.TYPE, ef.TYPE_ALTERN_ENTRY;
+and ac.EXTERNAL_SOURCE = 'Jax MTB');
 
--- repeat for name and nameAlternEntry fields
+-- repeat for name and nameAlternEntry fields (Fixed 1723 rows)
+update ENVIRONMENTAL_FACTOR ef
+set ef.NAME = ef.NAME_ALTERN_ENTRY
+where ef.NAME is null
+and ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
+where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
+and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
+and ac.EXTERNAL_SOURCE = 'Jax MTB');
 
+-- then null out the typeAlternEntry field (Fixed 1730 rows)
+update ENVIRONMENTAL_FACTOR ef
+set ef.NAME_ALTERN_ENTRY = null
+where ef.ENVIRONMENTAL_FACTOR_ID IN (
+select ef.ENVIRONMENTAL_FACTOR_ID 
+From environmental_factor ef, abs_cancer_model ac, carcinogen_exposure ce 
+where ef.ENVIRONMENTAL_FACTOR_ID = ce.ENVIRONMENTAL_FACTOR_ID
+and ce.ABS_CANCER_MODEL_ID = ac.ABS_CANCER_MODEL_ID
+and ac.EXTERNAL_SOURCE = 'Jax MTB');
