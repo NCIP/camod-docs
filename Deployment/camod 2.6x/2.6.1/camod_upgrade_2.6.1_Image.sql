d34ecef5831d9ee50123e0d745ffcf109bed5d51
Sima Pandya <pandyas@mail.nih.gov>
2010-04-05 02:59:29 +0000

    sql for image upgrade
    SVN-Revision: 5343

--- Deployment/camod 2.6x/2.6.1/camod_upgrade_2.6.1_Image.sql
+++ Deployment/camod 2.6x/2.6.1/camod_upgrade_2.6.1_Image.sql
@@ -4,31 +4,29 @@ set scan off;
 select
 case
     when URL like 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=%.sid' 
-       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/'), '.sid', '.tif')
+       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/%.tif' 
        then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model/%'
        then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
     else URL
 end t1
-from IMAGE_TEST; /* Your table here*/
+from IMAGE; /* Your table here*/
 
 
 -- use for updating
 
 
-update IMAGE_TEST set URL =
+update IMAGE set URL =
 case
     when URL like 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=%.sid' 
-       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/'), '.sid', '.tif')
+       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/%.tif' 
        then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model/%'
        then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
     else URL
 end;
-
-
 
 commit;
 
