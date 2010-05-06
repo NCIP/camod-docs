b3389821f34169c0a9fc6173fed13245c7ed72c7
Sima Pandya <pandyas@mail.nih.gov>
2010-05-06 15:01:26 +0000

    modified for caIMAGE upgrade
    SVN-Revision: 5423

--- Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
+++ Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
@@ -28,5 +28,20 @@ case
     else URL
 end;
 
+-- copy URL over completely for all tif images
+Update IMAGE i set i.THUMB_URL=i.URL
+where i.url LIKE '%.tif%';
+
+Update IMAGE i set i.THUMB_URL=i.URL
+where i.url LIKE '%.tif';
+
+-- modify inner string for thumb_url
+Update IMAGE i set i.THUMB_URL=replace (i.URL,'resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&','viewer.html?')  
+where i.url LIKE '%.tif%'; 
+
+Update IMAGE i set i.THUMB_URL=replace (i.URL,'resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&','viewer.html?')  
+where i.url LIKE '%.tif'; 
+
+
 commit;
 
