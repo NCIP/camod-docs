be99ef79d2b95c3e0842461264ab6b34f2d22bcd
Sima Pandya <pandyas@mail.nih.gov>
2010-05-06 18:52:04 +0000

    modified for #23750 Links to sid, gif, jpeg, jpg, tif images needs to be modified for caIMAGE upgrade URL contains "viewer.html?" string and thumb_url contains "resolver?..." string
    SVN-Revision: 5437

--- Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
+++ Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
@@ -4,11 +4,11 @@ set scan off;
 select
 case
     when URL like 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=%.sid' 
-       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
+       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/%.tif' 
-       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
+       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model/%'
-       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
+       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
     else URL
 end t1
 from IMAGE; /* Your table here*/
@@ -20,11 +20,11 @@ from IMAGE; /* Your table here*/
 update IMAGE set URL =
 case
     when URL like 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=%.sid' 
-       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
+       then replace(replace(SUBSTR(URL, 1, INSTR(URL, ';')), 'http://caimage.nci.nih.gov/lizardtech//iserv/getthumb?cat=Model&img=', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/'), '.sid', '.tif')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/%.tif' 
-       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
+       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/GeneticConstruct/', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/GeneticConstruct/')
     when URL like 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model/%'
-       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
+       then replace(URL, 'http://caimage.nci.nih.gov/lizardtech/Model_Images/Model', 'http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/viewer.html?rft_id=http://ncias-d330-v.nci.nih.gov:19080/adore-djatoka/images/caimage/Images/images/Model_Images/Model/')
     else URL
 end;
 
@@ -36,10 +36,10 @@ Update IMAGE i set i.THUMB_URL=i.URL
 where i.url LIKE '%.tif';
 
 -- modify inner string for thumb_url
-Update IMAGE i set i.THUMB_URL=replace (i.URL,'resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&','viewer.html?')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'viewer.html?','resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&')  
 where i.url LIKE '%.tif%'; 
 
-Update IMAGE i set i.THUMB_URL=replace (i.URL,'resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&','viewer.html?')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'viewer.html?','resolver?url_ver=Z39.88-2004&svc_id=info:lanl-repo/svc/getRegion&svc_val_fmt=info:ofi/fmt:kev:mtx:jpeg2000&svc.scale=100&')  
 where i.url LIKE '%.tif'; 
 
 
