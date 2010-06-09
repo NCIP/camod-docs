517cb238299b8f93570f697b403b039dfa749e0e
Sima Pandya <pandyas@mail.nih.gov>
2010-06-09 17:49:34 +0000

    updated url for Qa tier from imageserver-dev to imageserver
    SVN-Revision: 5473

--- Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
+++ Deployment/camod 2.6x/2.6.1/camod_2.6.1_Image.sql
@@ -43,47 +43,47 @@ Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'viewer.html?','resolver?url
 where i.url LIKE '%.tif'; 
 
 -- update url on QA and stage to point to caIMAGE prod tier
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.JPG';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.jpg';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.png';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.gif';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.jpeg';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.tif';
 
-Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.URL=replace (i.URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.url LIKE '%.tif%';
 
 -- update thumb_url on QA and stage to point to caIMAGE prod tier
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.JPG';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.jpg';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.png';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.gif';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.jpeg';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.tif';
 
-Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-stage.nci.nih.gov','http://imageserver.nci.nih.gov')  
+Update IMAGE i set i.THUMB_URL=replace (i.THUMB_URL,'http://imageserver-dev.nci.nih.gov','http://imageserver.nci.nih.gov')  
 where i.THUMB_URL LIKE '%.tif%';
 
 
